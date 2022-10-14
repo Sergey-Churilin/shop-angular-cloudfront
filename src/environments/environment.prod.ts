2443ba623cf49fa254a493e4cee6ea5f556ab5da
Siarhei Churylin <siarhei_churylin@epam.com>
2022-10-14 12:34:12 +0300

    feat: update path to data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://wzw29zbf2a.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://wmznl4fm5j.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
     bff: false,
